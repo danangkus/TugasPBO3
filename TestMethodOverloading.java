9a1dd6d1bfc3dd83c5f7c75087aef63e09c6f64d
danangkus <kdanang20@yahoo.co.id>
2015-04-07 15:39:58 +0700

    Tugas PBO 3 Method Overloading 2
    Ketambahan 1 method overload

--- TestMethodOverloading.java
+++ TestMethodOverloading.java
@@ -8,10 +8,14 @@ public class TestMethodOverloading{
 	public static int average(int n1, int n2, int n3){
 		return (n1+n2+n3)/3;
 	}
+	public static int average(int n1, int n2, int n3, int n4){
+		return (n1+n2+n3+n4)/4;
+	}
 	public static void main(String[] args){
 		System.out.println(average(1,2));
 		System.out.println(average(1.0,2.0));
 		System.out.println(average(1,2,3));
 		System.out.println(average(1.0,2));
+		System.out.println(average(1,2,3,4));
 	}
 }
